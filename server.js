78fe56aa83dacf28f96ce522871b34a02de27e3a
Maximilian Rice <maximilian.rice1@gmail.com>
2021-11-11 15:52:02 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("./db/connection")
+const morgan = require("morgan")
+const cors = require("cors")
+const app = express()
+const {PORT=7777} = process.env
+
+
+// TEST ROUTE
+app.get("/", (req, res) => res.send("hello world"))
+
+
+// LISTENER
+app.listen(PORT, () => console.log(`port running on ${PORT}`))
