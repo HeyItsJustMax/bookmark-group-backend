2054a4277c5bd7597b23483cb8d52a46ed35724b
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-11-13 14:49:43 -0500

    addnewroute - added a new route

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const {PORT=7777} = process.env
 // TEST ROUTE
 app.get("/", (req, res) => res.send("hello world"))
 
+//  New route goes here Greg
 
 // LISTENER
 app.listen(PORT, () => console.log(`port running on ${PORT}`))
